5ee3e6bc77538c2d60fe7ca050b084412579abfc
marcodldev <marcodelisi2@gmail.com>
2022-11-21 16:35:23 +0100

    api email + array push

--- main.js
+++ main.js
@@ -1,10 +1,25 @@
 var app = new Vue({
     el: '#root',
     data: {
-      message: 'Hello Vue!'
+        message: ' ',
+        email_array: [],
     },
     methods: {
+        generaMail() {
 
+            for ( i = 0; i < 10; i++) {
+
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                    .then(response => {
+                        
+                        this.email_array.push(response.data.response)
+                       
+                    })
+            }
+
+            console.log(this.email_array)
+
+        }
     },
-    
-  })
+
+})
